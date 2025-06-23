04682d54736bd39eae1822d380fcce9dd6f09e39
Alice Smith <alice@webage.com>
2025-06-23 11:17:35 -0400

    Made changes to planet.js and planet.html

--- guided_project_1_starter/guided_project_1/planet.js
+++ guided_project_1_starter/guided_project_1/planet.js
@@ -9,10 +9,10 @@ const baseUrl = `http://localhost:9001/api`;
 // Runs on page load
 addEventListener('DOMContentLoaded', () => {
   nameH1 = document.querySelector('h1#name');
-  birthYearSpan = document.querySelector('span#birth_year');
-  massSpan = document.querySelector('span#mass');
-  heightSpan = document.querySelector('span#height');
-  homeworldSpan = document.querySelector('span#homeworld');
+  terrainSpan = document.querySelector('span#terrain');
+  climateSpan = document.querySelector('span#climate');
+  diameterSpan = document.querySelector('span#diameter');
+  charactersUl = document.querySelector('#characters>ul');
   filmsUl = document.querySelector('#films>ul');
   const sp = new URLSearchParams(window.location.search)
   const id = sp.get('id')
@@ -33,20 +33,21 @@ async function getPlanet(id) {
 
 }
 async function fetchPlanet(id) {
-  let planetUrl = `${baseUrl}/planet/${id}`;
+  let planetUrl = `${baseUrl}/planets/${id}`;
   return await fetch(planetUrl)
     .then(res => res.json())
 }
 
 async function fetchCharacter(planet) {
-  const url = `${baseUrl}/characters/${planet?.character}`;
+  const url = `${baseUrl}/planets/${planet?.id}/characters`;
   const character = await fetch(url)
     .then(res => res.json())
+    console.log(character)
   return character;
 }
 
 async function fetchFilms(planet) {
-  const url = `${baseUrl}/characters/${planet?.id}/films`;
+  const url = `${baseUrl}/planets/${planet?.id}/films`;
   const films = await fetch(url)
     .then(res => res.json())
   return films;
@@ -55,10 +56,12 @@ async function fetchFilms(planet) {
 const renderPlanet = planet => {
   document.title = `SWAPI - ${planet?.name}`;  // Just to make the browser tab say their name
   nameH1.textContent = planet?.name;
-  heightSpan.textContent = planet?.height;
-  massSpan.textContent = planet?.mass;
-  birthYearSpan.textContent = planet?.birth_year;
-  characterSpan.innerHTML = `<a href="/planet.html?id=${planet?.character.id}">${planet?.character.name}</a>`;
+  terrainSpan.textContent = planet?.terrain;
+  climateSpan.textContent = planet?.climate;
+  diameterSpan.textContent = planet?.diameter;
   const filmsLis = planet?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
   filmsUl.innerHTML = filmsLis.join("");
+  const charactersLis = planet?.chracters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
+  charactersUl.innerHTML = charactersLis.join("");
+  
 }
